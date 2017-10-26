390cf0253700d19d968ba48a319f3be1dd71861d
Mateusz <mateusz.gibki@gmail.com>
2017-10-26 22:45:24 +0200

    adding new project files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.toggle').click(function(){
+		$('.toggle').toggleClass('active')
+	})
+	$('.toggle').click(function(){
+		$('.box').toggleClass('active')
+	})
+});
